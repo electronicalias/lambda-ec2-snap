ffd8667c980f5a1c415730ca09d570d779d634ef
Philip Smith <philip.smith@nordcloud.com>
2016-07-10 18:18:18 +0100

    Adding daily/monthly backups

--- scripts/lambsnapper.py
+++ scripts/lambsnapper.py
@@ -1,13 +1,18 @@
 import boto3
 import logging
+from datetime import datetime
 
 logging.getLogger().setLevel(logging.DEBUG)
 
 
 def lambda_handler(event, context):
-    
+
+    dtstamp = datetime.today()
+    dstamp = dtstamp.date()
+
     pname = event['Product']
     rname = event['Region']
+    period = event['Period']
     
     ec2 = boto3.client('ec2', rname)
     ecsnap = boto3.resource('ec2', rname)
@@ -19,13 +24,25 @@ def lambda_handler(event, context):
         )
         return data['SnapshotId']
     
-    def tag_snap(snap_id):
+    def tag_snap(snap_id, disk_id, instance_id):
         snapshot = ecsnap.Snapshot(snap_id)
         tag = snapshot.create_tags(
             Tags=[
                 {
                     'Key': 'Name',
-                    'Value': pname + '-something'
+                    'Value': str(dstamp) + '-' + period + '-' + pname
+                },
+                {
+                    'Key': 'Date',
+                    'Value': str(dstamp)
+                },
+                {
+                    'Key': 'Device',
+                    'Value': disk_id
+                },
+                {
+                    'Key': 'InstanceId',
+                    'Value': instance_id
                 },
             ]
         )
@@ -52,4 +69,4 @@ def lambda_handler(event, context):
                 print "InstanceId: %s \nVolumeName: %s\nDriveLetter: E" % (
                     instance['InstanceId'], Name)
                 snapid = create_snap(dev['Ebs']['VolumeId'])
-                tag_snap(snapid)
+                tag_snap(snapid, Name, instance['InstanceId'])
